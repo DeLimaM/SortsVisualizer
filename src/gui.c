f66ab88d7db6fe8a610c116df57061c0e3423e42
DeLimaM <martin.delimaa@gmail.com>
2024-11-30 23:36:46 +0100

    GUI enhancements

--- src/gui.c
+++ src/gui.c
@@ -4,33 +4,55 @@
 #include "sortviz.h"
 
 int width;
+int array_size;
+int bar_width;
+int bar_height_multiplier;
+
+SortType type;
 
 void drawBarGUI(int index, int value, Color color) {
   if (index < 0)
     return;
 
-  int max_bar_height = GetScreenHeight() - 100;
-  int bar_height = (value * max_bar_height) / GetScreenWidth();
-  DrawRectangle(index, GetScreenHeight() - bar_height, 1, bar_height, color);
-  DrawRectangle(index, INDICATORS_ZONE_HEIGHT, 1,
+  int max_bar_height = GetScreenHeight() - INDICATORS_ZONE_HEIGHT;
+  int bar_height =
+      ((value * max_bar_height) / GetScreenWidth()) * bar_height_multiplier;
+  DrawRectangle(index * bar_width, GetScreenHeight() - bar_height, bar_width,
+                bar_height, color);
+  DrawRectangle(index * bar_width, INDICATORS_ZONE_HEIGHT, bar_width,
                 GetScreenHeight() - bar_height, BACKGROUND_COLOR);
 }
 
 void drawIndicatorsGUI(SortParamsUnion *params) {
-  const char *stateStr = params->base.state == SORT_STATE_RUNNING ? "RUNNING"
-                         : params->base.state == SORT_STATE_FINISHED
-                             ? "FINISHED"
-                         : params->base.state == SORT_STATE_PAUSED ? "PAUSED"
-                                                                   : "IDLE";
+  const char *state_str = params->base.state == SORT_STATE_RUNNING ? "RUNNING"
+                          : params->base.state == SORT_STATE_FINISHED
+                              ? "FINISHED"
+                          : params->base.state == SORT_STATE_PAUSED ? "PAUSED"
+                                                                    : "IDLE";
 
-  DrawText(TextFormat("State: %s", stateStr), 10, 10, INDICATORS_FONT_SIZE,
+  const char *type_str = params->base.type == BUBBLE      ? "BUBBLE"
+                         : params->base.type == SELECTION ? "SELECTION"
+                         : params->base.type == INSERTION ? "INSERTION"
+                         : params->base.type == MERGE     ? "MERGE"
+                         : params->base.type == QUICK     ? "QUICK"
+                                                          : "NONE";
+
+  DrawText(TextFormat("State: %s", state_str), INDICATORS_MARGIN,
+           INDICATORS_MARGIN, INDICATORS_FONT_SIZE, WHITE);
+  DrawText(TextFormat("Type: %s", type_str), INDICATORS_MARGIN,
+           INDICATORS_MARGIN + INDICATORS_FONT_SIZE, INDICATORS_FONT_SIZE,
            WHITE);
-  DrawText(TextFormat("Comparisons: %d", params->base.comparisons), 10,
-           10 + INDICATORS_FONT_SIZE, INDICATORS_FONT_SIZE, BAR_COLOR);
-  DrawText(TextFormat("Swaps: %d", params->base.swaps), 10,
-           10 + 2 * INDICATORS_FONT_SIZE, INDICATORS_FONT_SIZE, RED);
-  DrawText(TextFormat("Inserts: %d", params->base.inserts), 10,
-           10 + 3 * INDICATORS_FONT_SIZE, INDICATORS_FONT_SIZE, GREEN);
+
+  DrawText(TextFormat("Comparisons: %d", params->base.comparisons),
+           INDICATORS_MARGIN + INDICATORS_WIDTH, INDICATORS_MARGIN,
+           INDICATORS_FONT_SIZE, BAR_COLOR);
+  DrawText(TextFormat("Swaps: %d", params->base.swaps),
+           INDICATORS_MARGIN + INDICATORS_WIDTH,
+           INDICATORS_MARGIN + INDICATORS_FONT_SIZE, INDICATORS_FONT_SIZE, RED);
+  DrawText(TextFormat("Inserts: %d", params->base.inserts),
+           INDICATORS_MARGIN + INDICATORS_WIDTH,
+           INDICATORS_MARGIN + 2 * INDICATORS_FONT_SIZE, INDICATORS_FONT_SIZE,
+           GREEN);
 
   DrawFPS(width - 90, 10);
 }
@@ -39,21 +61,31 @@ void drawFullArrayGUI(SortParamsUnion *params) {
   int *array = params->base.array;
   int size = params->base.size;
   for (int i = 0; i < size; i++) {
-    drawBarGUI(i, array[i], WHITE);
+    if (i == params->base.swap_params.index1 ||
+        i == params->base.swap_params.index2) {
+      drawBarGUI(i, array[i], RED);
+    } else if (i == params->base.insert_params.index) {
+      drawBarGUI(i, array[i], GREEN);
+    } else {
+      drawBarGUI(i, array[i], BAR_COLOR);
+    }
   }
   drawIndicatorsGUI(params);
 }
 
-void doSortInGUI(SortType type, int sleep_time) {
+void doSortInGUI(SortType type, int sleep_time, int array_size) {
   InitWindow(WINDOW_DEFAULT_WIDTH, WINDOW_DEFAULT_HEIGHT, WINDOW_TITLE);
   SetWindowState(FLAG_WINDOW_RESIZABLE);
   SetTargetFPS(60);
 
   width = GetScreenWidth();
+  bar_width = width / array_size;
+  bar_height_multiplier = width / array_size;
 
   SortParamsUnion params;
-  initBaseParams(&params.base, width, sleep_time, type);
+  initBaseParams(&params.base, array_size, sleep_time, type);
 
+  type = params.base.type;
   double lastUpdate = GetTime();
 
   while (!WindowShouldClose()) {
